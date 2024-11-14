bc4d94ba841af9376c34bff43d18abc7e22a5ca3
0xgouda <ahmedabdelmonaem@std.mans.edu.eg>
2024-11-15 00:03:25 +0200

    fix: used SLEEP_SECONDS instead of 5 :)

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from time import sleep
 from platform import system
 from subprocess import run
 
-SLEEP_MINUTES = 25
+SLEEP_MINUTES = 30
 SLEEP_SECONDS = SLEEP_MINUTES * 60
 HOST = system()
 NOTIFY_TITLE = "Protect Your Eyes!"
 NOTIFY_BODY = "Look at Something 20 feet away for 20 seconds"
 
 while (True):
-    sleep(5)
+    sleep(SLEEP_SECONDS)
     if (HOST == "Linux"):
         run(['notify-send', NOTIFY_TITLE, NOTIFY_BODY])
     elif (HOST == "Darwing"):
