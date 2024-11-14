bd2a5abd724fa615f3b498fd359c34fab1a76f31
0xgouda <ahmedabdelmonaem@std.mans.edu.eg>
2024-11-15 01:56:20 +0200

    added rest-time/sleep-time CLI args

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from time import sleep
 from platform import system
 from subprocess import run
-from sys import argv
+import argparse
 
-if ('-h' or '--help' in argv):
-    print('-h or --help: show help menu\nUsage:\n\tpython3 main.py --rest-time=20s --sleep-time=30m\n\tpython3 main.py # will default to 20s rest time & 30m sleep time')
-    exit()
+parser = argparse.ArgumentParser(prog="python3 main.py",
+                                description="Eye Rest Notifier Script", 
+                                epilog="Note: Time values default to seconds if no unit (s/m/h) is specified.")
+parser.add_argument('--rest-time', type=str, default='20s', help='Specify the rest time (default: 20s)')
+parser.add_argument('--sleep-time', type=str, default='30m', help='Specify the sleep time (default: 30m)')
+args = parser.parse_args()
 
+def parse_time_in_seconds(time):
+    if (time[-1] == 's'):
+        return int(time[:-1])
+    elif (time[-1] == 'm'):
+        return int(time[:-1]) * 60
+    elif (time[-1] == 'h'):
+        return int(time[:-1]) * 3600
+    
+    return int(time)
 
-SLEEP_MINUTES = 30
-SLEEP_SECONDS = SLEEP_MINUTES * 60
-REST_SECONDS = 20
-HOST = system()
+SLEEP_SECONDS = parse_time_in_seconds(args.sleep_time)
+REST_SECONDS = parse_time_in_seconds(args.rest_time)
 
 REST_NOTIFY_TITLE = "Protect Your Eyes!"
 REST_NOTIFY_BODY = f"Look at Something 6 meters away for {REST_SECONDS} seconds"
@@ -19,12 +29,12 @@ REST_NOTIFY_BODY = f"Look at Something 6 meters away for {REST_SECONDS} seconds"
 WORK_NOTIFY_TITLE = "Eye Rest End"
 WORK_NOTIFY_BODY = "Time to get Back to Work" 
 
+HOST = system()
 def command(Title, Body):
     if (HOST == "Linux"):
         return ['notify-send', Title, Body]
     elif (HOST == "Darwin"):
         return ['osascript', '-e', f"display notification {Body} with title {Title}"]
-
 
 while (True):
     sleep(SLEEP_SECONDS)
